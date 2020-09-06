ba66af5c91842091be64cbe9bb6269d59e2ee696
Aleydon <aleydonroberto@gmail.com>
2020-09-06 03:17:07 -0300

    added form and new styles

--- src/Styles/styled.js
+++ src/Styles/styled.js
@@ -5,16 +5,35 @@ import styled from 'styled-components';
 
 
 export const Title = styled.h1`
+    display: flex;
+    flex-direction: row;
     color: #f00;
     font-style: italic;
-
+    margin: 20px;
 `;
 
 
 export const SubTitle = styled.h3`
     color: #fff;
+    margin-top: 10px;
 `;
 
 
 export const Container = styled.div`
+    background: #fff;
+    border-radius: 10px;
+    margin: 60px auto;
+    padding: 30px;
+    width: 60%;
+`;
+
+
+
+export const SearchButton = styled.button`
+    width: 90px;
+`;
+
+
+
+export const Form = styled.form`
 `;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Title, SubTitle, Container } from '../../Styles/styled';
-import { FaGithub } from 'react-icons/fa';
+import { Title, SubTitle, Container, Form, SearchButton } from '../../Styles/styled';
+import { FaGithub, FaSearch } from 'react-icons/fa';
 
 
 
@@ -8,8 +8,13 @@ import { FaGithub } from 'react-icons/fa';
 function Main() {
   return(
       <Container>
-          <Title><FaGithub size={30} color="#fff" />Aleydon Repositories</Title>
+          <Title><FaGithub size={30} color="#000"  />Aleydon Repositories</Title>
           <SubTitle>Sub Title</SubTitle>
+
+          <Form>
+            <input type="text" placeholder="Type here your Repository" style={{width: 400}} />
+            <SearchButton><FaSearch size={16} /></SearchButton>
+          </Form>
       </Container>
   )
 }
