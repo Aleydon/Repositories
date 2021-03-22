75fee121d2367b78392a47dee1bd69d7942e18e0
Aleydon <aleydonroberto@gmail.com>
2021-03-22 18:19:07 -0300

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     browser: true,
     // es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,14 +16,17 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': ['error'],
     'import/no-named-as-default': 0,
     'import/extensions': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'no-console': 'off',
     'import/no-unresolved': [2, { caseSensitive: true }],
-    'eslint-disable react/no-array-index-key': true,
+    'eslint-disable react/no-array-index-key': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'react/prop-types': 0,
   },
 };
