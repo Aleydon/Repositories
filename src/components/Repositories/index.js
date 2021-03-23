d99b4336e1b4dfd7101a18809addd4d353522269
Aleydon <aleydonroberto@gmail.com>
2021-03-23 00:58:09 -0300

    Added Data api

--- src/components/Repositories/index.js
+++ src/components/Repositories/index.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Container } from './style';
+import { Container, Loading, Owner } from './style';
 import api from '../../services/api';
 
 function Repositories({ match }) {
+  const [repoData, setRepoData] = useState([]);
+  const [issueData, setIssueData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     async function loadRepositoryDetails() {
+      // Get repositorie and issues via params
       const repoDetail = decodeURIComponent(match.params.repositories);
       const [repositorieData, issuesData] = await Promise.all([
         api.get(`/repos/${repoDetail}`),
@@ -16,14 +21,26 @@ function Repositories({ match }) {
           },
         }),
       ]);
-      console.log(repositorieData.data, issuesData.data);
+      setRepoData(repositorieData.data);
+      setIssueData(issuesData.data);
+      setLoading(false);
     }
     loadRepositoryDetails();
   }, [match]);
 
+  if (loading) {
+    return <Loading style={{ color: 'white' }}>Testing...</Loading>;
+  }
+
   return (
     <Container>
-      <h3>Repositories {decodeURIComponent(match.params.repositories)}</h3>
+      <Owner>
+        <img src={repoData.owner.avatar_url} alt={repoData.name} />
+        <h1>Repositorie: {repoData.name} </h1>
+        <h3>URL: {repoData.url} </h3>
+        <h4>Description: {repoData.description}</h4>
+        <h5>{issueData.data}</h5>
+      </Owner>
     </Container>
   );
 }
