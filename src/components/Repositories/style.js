194dddb1226a199d5537c3ef86fd88cc1f5297e6
Aleydon <aleydonroberto@gmail.com>
2021-03-23 00:58:49 -0300

    Added initial style for Repositorie route

--- src/components/Repositories/style.js
+++ src/components/Repositories/style.js
@@ -8,3 +8,29 @@ export const Container = styled.div`
   width: 73%;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
 `;
+
+export const Loading = styled.div`
+  color: 'white';
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    margin: 20px 0;
+  }
+
+  h1 {
+    color: #f00;
+    font-size: 30px;
+  }
+`;
